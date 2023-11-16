23b44a0d6338b1c8d2c84610d6d77423724d772a
Jinhyup-Mok <mokjh1117@gmail.com>
2023-11-16 13:40:44 +0900

    Create CorsConfig for Spring Security Edit SecurityConfig - corsFilter and JWT Filter

--- src/main/java/mobileProgramming/hmhBackend/join/security/SecurityConfig.java
+++ src/main/java/mobileProgramming/hmhBackend/join/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package mobileProgramming.hmhBackend.join.security;
 
 import lombok.RequiredArgsConstructor;
+import mobileProgramming.hmhBackend.join.jwt.JwtAuthenticationFilter;
+import mobileProgramming.hmhBackend.join.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // Spring Security를 사용하여 보안 구성을 정의
 // URL 패턴들에 대한 접근을 설정하고, 기본 인증 방식, 세션 관리 및 암호화 방식을 정의하여 Spring Security 구성을 제공.
@@ -19,27 +22,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CorsConfig corsConfig;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeRequests(authorize -> authorize
-                        .antMatchers("/v2/api-docs", "/v3/api-docs/**", "/configuration/ui", "/swagger-resources/**",
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v2/api-docs", "/v3/api-docs/**", "/configuration/ui", "/swagger-resources/**",
                                 "/configuration/security", "/swagger-ui.html", "/webjars/**", "/file/**", "/image/**",
                                 "/swagger/**", "/swagger-ui/**", "/h2/**").permitAll()
-                        .antMatchers(HttpMethod.OPTIONS).permitAll()
-                        .antMatchers("/member/email", "/member/{id}", "/member/login", "/member/join", "/member/insertImage/**").permitAll()
-                        .antMatchers("/member/follow/**").permitAll() // follow DB 접근 권한 설정, 여기에 CRUD mapping 권한 추가 -> .permitAll()
-                        .antMatchers("/member/place/**").permitAll()
-                        .antMatchers("/member/posting/**").permitAll()
-                        .antMatchers("/member/todoList/**").permitAll()
-                        .antMatchers("/member").hasRole("USER")
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form -> form.disable())
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .cors().disable()
-                .csrf().disable()
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/member/email", "/member/{id}", "/member/login", "/member/join", "/member/insertImage/**").permitAll()
+                .antMatchers("/member/follow/**").permitAll() // follow DB 접근 권한 설정, 여기에 CRUD mapping 권한 추가 -> .permitAll()
+                .antMatchers("/member/place/**").permitAll()
+                .antMatchers("/member/posting/**").permitAll()
+                .antMatchers("/member/todoList/**").permitAll()
+                .antMatchers("/member").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(corsConfig.corsFilter())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
@@ -48,4 +57,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+
 }
